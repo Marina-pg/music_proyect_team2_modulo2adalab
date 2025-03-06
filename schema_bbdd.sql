559d67b5e4f9be34d135dffd6499796a256f1bb5
marianela-gomez <marianela.gomez.linkedin@gmail.com>
2025-03-06 13:31:09 +0100

    2005-2009 and schema bbdd added

---
+++ schema_bbdd.sql
@@ -0,0 +1,45 @@
+CREATE SCHEMA IF NOT EXISTS MusicStream;
+
+USE MusicStream;
+
+CREATE TABLE artistas (
+id_artista INT AUTO_INCREMENT PRIMARY KEY,
+nombre_artista VARCHAR(255)
+);
+
+CREATE TABLE años(
+id_lustro INT AUTO_INCREMENT PRIMARY KEY,
+rango_años VARCHAR(10) 
+);
+
+CREATE TABLE paises(
+id_pais INT AUTO_INCREMENT PRIMARY KEY,
+nombre_pais VARCHAR(50) 
+);
+
+CREATE TABLE canciones(
+id_cancion INT AUTO_INCREMENT PRIMARY KEY,
+cancion VARCHAR(100),
+genero VARCHAR(50),
+popularidad INT, 
+id_lustro INT,
+id_pais INT, 
+CONSTRAINT `fk_canciones_año`
+              FOREIGN KEY (id_lustro)
+              REFERENCES años(id_lustro),
+CONSTRAINT `fk_canciones_pais`
+              FOREIGN KEY (id_pais)
+              REFERENCES paises(id_pais)
+);
+
+CREATE TABLE artista_en_cancion(
+id_artista INT,
+id_cancion INT,
+PRIMARY KEY (id_artista, id_cancion),
+CONSTRAINT `fk_artista_cancion` 
+	FOREIGN KEY (id_artista)
+	REFERENCES artistas(id_artista),
+CONSTRAINT `fk_cancion_artista` 
+	FOREIGN KEY (id_cancion)
+	REFERENCES canciones(id_cancion)
+);
